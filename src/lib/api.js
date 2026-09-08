4a55431f4966cd062b7eb212d10afdec043f0441
Michelle Wambaya <michellewambaya@gmail.com>
2026-09-08 12:32:48 +0300

    Add delete method to api object

--- src/lib/api.js
+++ src/lib/api.js
@@ -45,6 +45,7 @@ export const api = {
   get: (path) => request(path),
   post: (path, body) => request(path, { method: 'POST', body: JSON.stringify(body) }),
   put: (path, body) => request(path, { method: 'PUT', body: JSON.stringify(body) }),
+  delete: (path) => request(path, { method: 'DELETE' }),
 };
 
 export async function getAccessToken() {
